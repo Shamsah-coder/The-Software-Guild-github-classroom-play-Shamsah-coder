5c6799a85607ddb7c79d683947fd3bb80cf54e6e
Shamsah-coder <rosemitha45@gmail.com>
2021-05-23 01:31:46 -0400

    Final Test in NetBeans

---
+++ Hello/src/main/java/FinalTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rose.hello;
+
+/**
+ *
+ * @author User
+ */
+public class FinalTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World, My Name is Rose");
+        System.out.println("My email is rosemitha45@gmail.com");
+        System.out.println("Todays Date is May 22nd");
+    }
+    
+}
+
