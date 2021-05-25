a3767604609299731901d82cdd46671ed7497892
Shamsah-coder <rosemitha45@gmail.com>
2021-05-25 03:34:43 -0400

    ABeginning in NetBeans

---
+++ PracticeProgramming/src/main/java/com/rose/foundationsbasics/ABeginning.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rose.foundationsbasics;
+
+/**
+ *
+ * @author User
+ */
+public class ABeginning {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello from The Guild");
+        System.out.println("Typing code is easier than I thoutht ...");
+        System.out.println("typity Typity Type!");
+        System.out.println("After I finish typing,");
+        System.out.println("I'll compile my code.");
+        System.out.println("Then when I run it,");
+        System.out.println("The console will print out all my brilliant words!");
+        System.out.println("And it all starts with Hello World...");
+        System.out.println("Thought I'd all that I used all the short cuts like"
+                + "psvm tab and sout tab, makes it a lot easier");
+        
+    }
+}
