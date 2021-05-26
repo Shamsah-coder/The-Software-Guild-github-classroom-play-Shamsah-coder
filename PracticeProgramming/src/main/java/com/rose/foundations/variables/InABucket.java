0d64122dfaea73b718a2bcbf385d1ba319c502b5
Shamsah-coder <rosemitha45@gmail.com>
2021-05-26 05:07:24 -0400

    InABucket in NetBeans

---
+++ PracticeProgramming/src/main/java/com/rose/foundations/variables/InABucket.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rose.foundations.variables;
+
+/**
+ *
+ * @author User
+ */
+public class InABucket {
+    public static void main(String[] args) {
+
+        // Declaring variables
+        String walrus;
+        double piesEaten;
+        // to use the variable, you have to put data IN it first!
+        walrus = "Sir Leroy Jenkins III";
+        piesEaten = 42.1;
+
+        System.out.println("Meet my pet walrus, " + walrus);
+        System.out.print("He was a bit hungry today, and ate this many pies : ");
+        System.out.println(piesEaten);
+    }
+}
