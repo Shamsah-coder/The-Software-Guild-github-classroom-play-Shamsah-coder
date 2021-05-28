407fcb735908f64acc932cc5d4b6fe8c5f7e7abc
Shamsah-coder <rosemitha45@gmail.com>
2021-05-28 02:17:19 -0400

    BestAdderEver in NetBeans

---
+++ PracticeProgramming/src/main/java/com/rose/foundations/variables/BestAdderEver.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rose.foundations.variables;
+
+/**
+ *
+ * @author User
+ */
+public class BestAdderEver {
+    public static void main(String[] args) {
+        //Declare variables
+        int men, women;
+        int babies, family;
+        men = 4;
+        women = 7;
+        babies = 12;
+        family = men + women + babies;
+        
+        System.out.println(men + ("+") + women + ("+") + babies + ("=")+ family);
+                
+    }
+}
