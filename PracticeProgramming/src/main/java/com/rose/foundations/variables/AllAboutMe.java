5762a891981e46f391262600a19dbbfd8afb51ec
Shamsah-coder <rosemitha45@gmail.com>
2021-05-27 23:35:04 -0400

    AllAboutMe in NetBeans

---
+++ PracticeProgramming/src/main/java/com/rose/foundations/variables/AllAboutMe.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rose.foundations.variables;
+
+/**
+ *
+ * @author User
+ */
+public class AllAboutMe {
+    public static void main(String[] args) {
+        int pets;
+        String name, favouriteFoodGroup, residence, whistlingAbility; 
+        
+        name = "My Name is Rose Mitha. ";
+        favouriteFoodGroup = "My favourite food group is fruits. ";
+        residence = "I live in an apartment. ";
+        whistlingAbility = "I try to whistle. ";
+        pets = 0;
+        
+        System.out.println(name + "" + favouriteFoodGroup + "" + residence + ""); 
+        System.out.println(("Good thing I have ") + pets + (" pets."));
+        
+        
+        
+    }
+    
+}
