f3ec5abedb15a979d5e2d9797c3da2760657d414
Shamsah-coder <rosemitha45@gmail.com>
2021-05-28 02:01:18 -0400

    MenuOfChampions in NetBeans

---
+++ PracticeProgramming/src/main/java/com/rose/foundations/variables/MenuOfChampions.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rose.foundations.variables;
+
+/**
+ *
+ * @author User
+ */
+public class MenuOfChampions {
+    public static void main(String[] args) {       
+        double Pizza;
+        float Pie;
+        float Omelet;
+        String Name, Symbol;
+        
+        Pizza = 12.55;
+        Pie = (float) 8.99;
+        Omelet = (float) 6.99;
+        Name = "Rosie's Eatery";
+        Symbol = "$";
+
+        System.out.println("");
+        System.out.println("............." +Name + "" + "....................");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Pizza............................." + Symbol +"" + Pizza + "");
+        System.out.println("Pie..............................." + Symbol + "" + Pie + " ");
+        System.out.println("Omelet............................" + Symbol + "" + Omelet + " ");
+       
+    }
+}               
+        
+
