18df6c80e12716ec8caf81c6fdbe0272962b28a8
Shamsah-coder <rosemitha45@gmail.com>
2021-06-01 12:02:21 -0400

    GuessMe in NetBeans

--- PracticeProgramming/src/main/java/com/rose/foundationsflowcontrol/ifs/GuessMe.java
+++ PracticeProgramming/src/main/java/com/rose/foundationsflowcontrol/ifs/GuessMe.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package co.rose.foundationsflowcontrol.ifs;
+package com.rose.foundationsflowcontrol.ifs;
 
 import java.util.Scanner;
 
